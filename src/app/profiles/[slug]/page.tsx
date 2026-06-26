e97a67f7849383a31ff8290b25290409b9a4ded2
Gregory Bowrin <46822434+gregorybowrin@users.noreply.github.com>
2026-06-25 22:31:01 -0400

    feat: add 'Photo credit:' prefix to image credits

--- src/app/profiles/[slug]/page.tsx
+++ src/app/profiles/[slug]/page.tsx
@@ -109,7 +109,7 @@ export default async function ProfileDetailPage({ params }: { params: { slug: st
 
             {figure.image_credit && (
               <div className="text-[11px] text-black px-2 italic text-center font-['Times_New_Roman',_Times,_serif] mt-2">
-                {figure.image_credit}
+                Photo credit: {figure.image_credit}
               </div>
             )}
 
